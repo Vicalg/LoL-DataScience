de0a6055f1e5fd9d92d741ba2e0466610b3b9101
Victor Guzmán <guzmanvictor1993@gmail.com>
2023-02-17 21:21:49 -0400

    Rewriting the functions for loading the data and getting the puuid. Now they use the path of the parent directory to find the corresponding files. Functions directory is added to syspath, right now that process is done manually at the notebook, I'll change that later.

--- Functions/Load_functions.py
+++ Functions/Load_functions.py
@@ -1,4 +1,5 @@
 import pickle
+import os
 
 # Function for loading the pickle file
 def load_file(filename: str) -> list:
@@ -10,16 +11,23 @@ def load_file(filename: str) -> list:
     Return:
     matches_list -- List with the data (list)
     """
-    with open(filename, "rb") as file:
+    path_data = os.path.dirname(os.getcwd()) + "\\Data\\" + filename 
+    with open(path_data, "rb") as file:
         matches_list = pickle.load(file)
     return matches_list
 
+def read_puuid(filename: str) -> str:
 
+    # Reading my puuid (Player Universally Unique IDentifiers) - If i need to use it
+    path = os.path.dirname(os.getcwd()) + "\\Keys\\" + filename
+    with open(path, "r") as file:
+        puuid = file.read()
+    return puuid
 
-def 
-
-for information in list_keys_info_participants:
-    df[information] = [matches_no_error[x]["info"]["participants"][index_puuid_match(matches_no_error[x])].get(information) for x in range(len(matches_no_error))]
+#def clean_matches()
+#
+#for information in list_keys_info_participants:
+#    df[information] = [matches_no_error[x]["info"]["participants"][index_puuid_match(matches_no_error[x])].get(information) for x in range(len(matches_no_error))]
 
 
 # Extract the information for a given player on a match
@@ -30,5 +38,3 @@ def index_puuid_match(match ,puuid):
     return: index value for the summoner with the given puuid (int)
     """
     return(match["metadata"]["participants"].index(puuid))
-
-
